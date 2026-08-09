283dbd123f38b75fb103287a94814d1c3badbbba
Tim275 <sagichdirnicht259@outlook.de>
2026-08-09 20:26:28 +0200

    tofu icon on devicons layer

--- dot_config/nvim/lua/plugins/tofu.lua
+++ dot_config/nvim/lua/plugins/tofu.lua
@@ -1,5 +1,14 @@
 return {
   {
+    -- Seitenleiste (snacks) rendert ueber devicons, nicht mini.icons
+    "nvim-tree/nvim-web-devicons",
+    opts = {
+      override_by_extension = {
+        tofu = { icon = "󰆦", color = "#e0af68", name = "OpenTofu" },
+      },
+    },
+  },
+  {
     "echasnovski/mini.icons",
     init = function()
       -- .tofu (OpenTofu) = gleiche Pipeline wie .tf: highlighting, terraformls, terraform_fmt
