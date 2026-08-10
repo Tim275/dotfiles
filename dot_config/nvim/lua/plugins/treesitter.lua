e597c176ff6f712ea51ae409349ddd8c0c7c958a
Tim275 <sagichdirnicht259@outlook.de>
2026-08-10 22:12:56 +0200

    treesitter parsers, drop fish linter

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      -- image.nvim/render-markdown warnen sonst bei jedem checkhealth
+      ensure_installed = { "css", "scss", "latex", "typst", "svelte", "vue", "norg" },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      -- kein fish installiert
+      opts.linters_by_ft.fish = nil
+    end,
+  },
+}
