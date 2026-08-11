fa69548056cbba604337bba97c205b4fff2421d8
Tim275 <sagichdirnicht259@outlook.de>
2026-08-11 11:18:25 +0200

    yaml icon default

--- dot_config/nvim/lua/plugins/icons.lua
+++ dot_config/nvim/lua/plugins/icons.lua
@@ -1,7 +1,6 @@
 local glyph = vim.fn.nr2char
 
 local cube = glyph(0xF01A6) -- nf-md-cube
-local yml = glyph(0xE6A8) -- nf-seti-yml
 local terraform = glyph(0xE69A) -- nf-seti-terraform
 
 return {
@@ -13,8 +12,6 @@ return {
     opts = {
       extension = {
         tofu = { glyph = cube, hl = "MiniIconsYellow" },
-        yaml = { glyph = yml, hl = "MiniIconsBlue" },
-        yml = { glyph = yml, hl = "MiniIconsBlue" },
         tfvars = { glyph = terraform, hl = "MiniIconsPurple" },
         tf = { glyph = terraform, hl = "MiniIconsPurple" },
       },
