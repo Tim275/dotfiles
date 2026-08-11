f0d0a9a22473f135deea16da2f03289307ad157c
Tim275 <sagichdirnicht259@outlook.de>
2026-08-11 15:11:03 +0200

    yaml icon: restore devicons override

--- dot_config/nvim/lua/plugins/icons.lua
+++ dot_config/nvim/lua/plugins/icons.lua
@@ -2,8 +2,19 @@ local glyph = vim.fn.nr2char
 
 local cube = glyph(0xF01A6) -- nf-md-cube
 local terraform = glyph(0xE69A) -- nf-seti-terraform
+local yml = glyph(0xE6A8) -- nf-seti-yml
 
 return {
+  {
+    -- Seitenleiste (snacks) rendert ueber devicons, nicht mini.icons
+    "nvim-tree/nvim-web-devicons",
+    opts = {
+      override_by_extension = {
+        yaml = { icon = yml, color = "#f7768e", name = "Yaml" },
+        yml = { icon = yml, color = "#f7768e", name = "Yml" },
+      },
+    },
+  },
   {
     "nvim-mini/mini.icons",
     init = function()
@@ -14,6 +25,8 @@ return {
         tofu = { glyph = cube, hl = "MiniIconsYellow" },
         tfvars = { glyph = terraform, hl = "MiniIconsPurple" },
         tf = { glyph = terraform, hl = "MiniIconsPurple" },
+        yaml = { glyph = yml, hl = "MiniIconsRed" },
+        yml = { glyph = yml, hl = "MiniIconsRed" },
       },
     },
     config = function(_, opts)
